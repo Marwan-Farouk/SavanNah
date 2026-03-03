75365e565513ab800199eb080845838302365176
Marwan-Farouk <marwanfarouk3284@gmail.com>
2026-03-03 02:06:26 +0200

    Created Category Controller and added views to create categories and list all categories

---
+++ SavanNah/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SavanNah.Contexts;
+using SavanNah.Models;
+
+namespace SavanNah.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public CategoryController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Categories.OrderBy(c => c.DisplayOrder).ToListAsync());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                await context.Categories.AddAsync(category);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
