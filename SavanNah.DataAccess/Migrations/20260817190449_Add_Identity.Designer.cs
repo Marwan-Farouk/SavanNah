d846997a1f3b04b176892a1f165da6340d72e4ce
Marwan-Farouk <marwanfarouk3284@gmail.com>
2026-08-21 16:09:27 +0300

    Add ASP.NET Core Identity and update Products table
    Introduced ASP.NET Core Identity with new migration files creating standard Identity tables using Guid keys. Updated AppDbContextModelSnapshot for new schema. Made Products.Image column nullable. Added EF Core Design package to project file.

--- SavanNah.DataAccess/Migrations/20260817190449_Add_Identity.Designer.cs
+++ SavanNah.DataAccess/Migrations/20260817190449_Add_Identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SavanNah.DataAccess.Contexts;
 
@@ -11,9 +12,11 @@ using SavanNah.DataAccess.Contexts;
 namespace SavanNah.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260817190449_Add_Identity")]
+    partial class Add_Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
